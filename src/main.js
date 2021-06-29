53756d302c72852a76225013e22c675799fbf52f
abrahamkivosh <abrahamkivosh@gmail.com>
2021-06-29 21:56:27 +0300

    initial

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
+// import './plugins/axios'
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './store'
+// import  Axios from 'axios'
 
-createApp(App).mount('#app')
+createApp(App).use(store).use(router).mount('#app')
